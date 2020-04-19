a9965c093a696a90c4e31079c15d632fc5c54702
Peter Veerman <peterkangveerman@gmail.com>
2020-04-19 16:06:38 -0700

    Add 4th round upstream distribution converter

--- src/3rd_round/main.cpp
+++ src/3rd_round/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/*  4TH ROUND FILE FORMAT
+    hand0,hand1,cc0,cc1,cc2,cc3,cc4,b0,b1,b2,mean
+    5,6,0,1,2,3,4,0,990,0,0.5
+    5,7,0,1,2,3,4,0,990,0,0.5
+    5,8,0,1,2,3,4,0,990,0,0.5
+*/
+
 int main() {
     printf("Hello world\n");
     return 0;
